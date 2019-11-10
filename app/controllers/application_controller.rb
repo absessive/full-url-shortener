90998b9132612ac41e5f3a685a4f106f6d7a5ed3
Ajit Chakrapani <absessive@gmail.com>
2019-11-10 12:56:02 -0500

    Prepare for Heroku deploy.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::API  
   NotAuthorized = Class.new(StandardError)
+
+  def fallback_index_html
+    render :file => 'public/index.html'
+  end
 end
